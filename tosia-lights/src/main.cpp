66b1c421c883134de61ada610f81ebe9dfc3e8d0
Marcin Nowak <Marcin.Nowak@sonyx.netr>
2018-07-16 18:36:59 +0200

    tosia lights update

--- tosia-lights/src/main.cpp
+++ tosia-lights/src/main.cpp
@@ -2,23 +2,28 @@
 
 #define FASTLED_ALLOW_INTERRUPTS 0
 #define FASTLED_ESP8266_NODEMCU_PIN_ORDER
+
 #include <FastLED.h>
 #include <MQTTClient.h>
-#include <ESP8266WiFi.h>
-//#include <WiFi.h>
+#include <WiFi.h>
 #include "wifi-password.h"
 
 WiFiClient net;
 MQTTClient client;
 
+#include <IRremote.h>
+
+uint16_t RECV_PIN = 23;
+IRrecv irrecv(RECV_PIN);
+decode_results results;
 
 // How many leds in your strip?
-#define NUM_LEDS 150
+#define NUM_LEDS 50
 
 // For led chips like Neopixels, which have a data line, ground, and power, you just
 // need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
 // ground, and power), like the LPD8806, define both DATA_PIN and CLOCK_PIN
-#define DATA_PIN 5
+#define DATA_PIN 22
 #define CLOCK_PIN 2
 
 int red = 10;
@@ -28,6 +33,10 @@ int blue = 10;
 int hue = 10;
 int saturation = 10;
 int brightness = 10;
+
+int oldHue = hue;
+int oldSaturation = saturation;
+int oldBrightness = brightness;
 
 int oldRed = red;
 int oldGreen = green;
@@ -40,139 +49,147 @@ int oldBlue = blue;
 // Define the array of leds
 CRGB leds[NUM_LEDS];
 
+void messageReceived(String &topic, String &payload)
+{
 
-void messageReceived(String &topic, String &payload) {
-  
-          if( topic == String("/light/tosia/hue") ) {
-                  if( payload == String("+") ){
-                    hue++;
-                  }
-                  else if( payload == String("-") ){
-                    hue--;
-                  }
-                  else{
-                    hue = payload.toInt();
-                  }
-                  client.publish("/light/tosia/ack","h: "+String(hue) + " s: " + String(saturation) + " b: " + String(brightness));
-          }
-          if( topic == String("/light/tosia/saturation") ) {
-                  if( payload == String("+") ){
-                    saturation++;
-                  }
-                  else if( payload == String("-") ){
-                    saturation--;
-                  }
-                  else{
-                    saturation = payload.toInt();
-                  }
-                  client.publish("/light/tosia/ack","h: "+String(hue) + " s: " + String(saturation) + " b: " + String(brightness));
-          }
-          if( topic == String("/light/tosia/brightness") ) {
-                  if( payload == String("+") ){
-                    brightness++;
-                  }
-                  else if( payload == String("-") ){
-                    brightness--;
-                  }
-                  else{
-                    brightness = payload.toInt();
-                  }
-  
-                  client.publish("/light/tosia/ack","h: "+String(hue) + " s: " + String(saturation) + " b: " + String(brightness));
-          }
-  
-          if( topic == String("/light/tosia/red") ) {
-                  red = payload.toInt();
-                  client.publish("/light/tosia/ack","red "+payload);
-          }
-          if( topic == String("/light/tosia/green") ) {
-                  red = payload.toInt();
-                  client.publish("/light/tosia/ack","green "+payload);
-          }
-          if( topic == String("/light/tosia/blue") ) {
-                  red = payload.toInt();
-                  client.publish("/light/tosia/ack","blue "+payload);
-          }
-          //show();
+  if (topic == String("/light/tosia/hue"))
+  {
+    hue = payload.toInt();
+    client.publish("/light/tosia/ack", "h: " + String(hue) + " s: " + String(saturation) + " b: " + String(brightness));
   }
-  
-void connect() {
-    while (WiFi.status() != WL_CONNECTED) {
-            delay(1000);
-    }
+  if (topic == String("/light/tosia/saturation"))
+  {
+    saturation = payload.toInt();
+    client.publish("/light/tosia/ack", "h: " + String(hue) + " s: " + String(saturation) + " b: " + String(brightness));
+  }
+  if (topic == String("/light/tosia/brightness"))
+  {
+    brightness = payload.toInt();
+    client.publish("/light/tosia/ack", "h: " + String(hue) + " s: " + String(saturation) + " b: " + String(brightness));
+  }
 
-    while (!client.connect("tosia-light")) {
-            delay(1000);
-    }
-    client.subscribe("/light/tosia/red");
-    client.subscribe("/light/tosia/green");
-    client.subscribe("/light/tosia/blue");
-    client.subscribe("/light/tosia/hue");
-    client.subscribe("/light/tosia/saturation");
-    client.subscribe("/light/tosia/brightness");
-
+  if (topic == String("/light/tosia/red"))
+  {
+    red = payload.toInt();
+    client.publish("/light/tosia/ack", "red " + payload);
+  }
+  if (topic == String("/light/tosia/green"))
+  {
+    red = payload.toInt();
+    client.publish("/light/tosia/ack", "green " + payload);
+  }
+  if (topic == String("/light/tosia/blue"))
+  {
+    red = payload.toInt();
+    client.publish("/light/tosia/ack", "blue " + payload);
+  }
+  //show();
 }
 
+void connect()
+{
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(1000);
+  }
 
-void setup() { 
-  LEDS.addLeds<WS2812B,DATA_PIN>(leds,NUM_LEDS,0);
+  while (!client.connect("tosia-light"))
+  {
+    delay(1000);
+  }
+  client.subscribe("/light/tosia/red");
+  client.subscribe("/light/tosia/green");
+  client.subscribe("/light/tosia/blue");
+  client.subscribe("/light/tosia/hue");
+  client.subscribe("/light/tosia/saturation");
+  client.subscribe("/light/tosia/brightness");
+}
+
+void setup()
+{
+  LEDS.addLeds<WS2812B, DATA_PIN>(leds, NUM_LEDS, 0);
   LEDS.setBrightness(250);
-    WiFi.mode(WIFI_STA);
-      WiFi.begin(ssid, pass);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, pass);
 
-        // Note: Local domain names (e.g. "Computer.local" on OSX) are not supported by Arduino.
-        // You need to set the IP address directly.
-        client.begin("10.10.1.3", net);
-        client.onMessage(messageReceived);
-      connect(); 
+  client.begin("10.10.1.3", net);
+  client.onMessage(messageReceived);
+  connect();
 
+  irrecv.enableIRIn();
+  //myReceiver.enableIRIn(); // Start the receiver
 }
 
-//void fadeall() { for(int i = 0; i < NUM_LEDS; i++) { leds[i].nscale8(250); } }
+void loop()
+{
 
+  while (true)
+  {
 
+    client.loop();
 
-void loop() { 
-  
-  while( true ){
-
-        client.loop();
-
-        if (!client.connected()) {
-                connect();
-        }
-
-    
-  // First slide the led in one direction
-  for(int i = 0; i < NUM_LEDS; i++) {
-    // Set the i'th led to red 
-    CHSV newColor = CHSV(hue, saturation, brightness);
-    hsv2rgb_rainbow(newColor,leds[i]);
-    // Show the leds
+    if (!client.connected())
+    {
+      connect();
     }
-    FastLED.show(); 
-    delay(5);
+
+    if (oldHue != hue || oldSaturation != saturation || oldBrightness != brightness)
+    {
+      /*if( hue > oldHue ){
+        oldHue++;
+      }
+      
+      if( hue < oldHue ){
+        oldHue--;
+      }
+
+      if( saturation > oldSaturation) {
+        oldSaturation++;
+      }
+
+      if( saturation < oldSaturation ){
+        oldSaturation--;
+      }
+
+      if( brightness > oldBrightness ){
+        oldBrightness++;
+      }
+
+      if( brightness < oldBrightness ){
+        oldBrightness--;
+      }
+      */
+      oldHue = hue;
+      oldBrightness = brightness;
+      oldSaturation = saturation;
+
+      // First slide the led in one direction
+      for (int i = 0; i < NUM_LEDS; i++)
+      {
+        // Set the i'th led to red
+        CHSV newColor = CHSV(oldHue, oldSaturation, oldBrightness);
+        hsv2rgb_rainbow(newColor, leds[i]);
+        // Show the leds
+      }
+      FastLED.show();
+    }
+
+    if (irrecv.decode(&results))
+    {
+      if (results.decode_type == NEC)
+      {
+        uint64_t number = results.value;
+        unsigned long long1 = (unsigned long)((number & 0xFFFF0000) >> 16);
+        unsigned long long2 = (unsigned long)((number & 0x0000FFFF));
+
+        String hex = String(long1, HEX) + String(long2, HEX); // six octets
+
+        client.publish("/light/tosia/irReceiver", hex);
+      }
+
+      irrecv.resume(); // Receive the next value
+    }
+
+    delay(30);
   }
 }
-
-
-
-
-
-/*
-void setup() {
-    // put your setup code here, to run once:
-    pinMode(1, OUTPUT);
-}
-
-void loop() {
-    // put your main code here, to run repeatedly:
-    digitalWrite(1, HIGH);   // Turn on the LED
-    
-      delay(1000);              // Wait for one second
-    
-      digitalWrite(1, LOW);    // Turn off the LED
-    
-      delay(1000);     
-}
-*/
